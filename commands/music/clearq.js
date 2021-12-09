d05cdb32f1153a35f55144c43a532e0be2b2c4ca
Felix platzer <feliplatzer06@gmail.com>
2021-12-09 20:43:18 +0100

    Create clearq.js

---
+++ commands/music/clearq.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    name: "clearq",
+    aliases: ['cq', 'clearqueue'],
+    cooldown: 1000 * 1,
+    description: "clears the queue",
+
+
+    async run(client, message, args) {
+        const queue = client.distube.getQueue(message)
+        if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+        if (!queue) return message.reply("There is nothing to clear!");
+
+        queue.songs = [queue.songs[0]]
+
+        await message.reply("Cleared")
+    }
+}
