b812d6c1166da1dd108e99bac1e18773c6bcb58c
Felix platzer <feliplatzer06@gmail.com>
2021-12-10 13:07:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ client.on('error', console.error);
 client.once('ready', () => {
 
     console.log(`\nBot is Online! \nThere are: ${commandCount - 1} commands\n`)
+    client.user.setActivity('discord.js', { type: 'WATCHING' });
 })
 
 client.on('messageCreate', message => {
