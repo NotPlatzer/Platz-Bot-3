962637b267f313737bbe5e710e1c6fa540922189
Felix platzer <feliplatzer06@gmail.com>
2021-12-11 20:01:33 +0100

    added leave.js

---
+++ commands/music/leave.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    name: "leave",
+    aliases: ['leave', 'die'],
+    cooldown: 1000 * 5,
+    description: "leaves the vc",
+
+
+    async run(client, message, args) {
+        
+        if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+        
+        message.guild.me.voice.channel.leave(); 
+        await message.reply("**Left the Channel**");
+    }
+}
