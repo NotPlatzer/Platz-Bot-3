0431af7b4a138c1c490f764e2973368397f89dec
Felix platzer <feliplatzer06@gmail.com>
2021-12-12 23:01:41 +0100

    ok

---
+++ commands/managment/changeindex.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const mongoose = require('mongoose');
+const Guild = require('./db_models/guild');
+
+module.exports = {
+    name: "changeindex",
+    aliases: ['ci'],
+    cooldown: 1000 * 5,
+    description: "ping command",
+
+
+    async run(client, message, args) {
+        
+        Guild.findOneAndUpdate({
+            id: message.guildid
+        }, {
+            prefix: '.'
+        })
+
+    }
+    
+}
