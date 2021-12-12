2765156383f8f7494d6a275633f6caf597e3c8e7
Felix platzer <feliplatzer06@gmail.com>
2021-12-12 11:53:28 +0100

    dd

--- db_models/guild.js
+++ db_models/guild.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const guildSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
     prefix: {
         type: String,
         required: true
