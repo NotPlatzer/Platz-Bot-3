888343987fa20f4f0e9caaf8d0c396e4d340c34e
Felix platzer <feliplatzer06@gmail.com>
2021-12-12 23:03:54 +0100

    ok

--- commands/managment/changeindex.js
+++ commands/managment/changeindex.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const mongoose = require('mongoose');
-const Guild = require('./db_models/guild');
+const Guild = require('db_models\guild.js');
 
 module.exports = {
     name: "changeindex",
