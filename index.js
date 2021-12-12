01ee01b08f4988b63d6efec8a4ce9b491bc74a3b
Felix platzer <feliplatzer06@gmail.com>
2021-12-12 11:50:25 +0100

    ddd

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ client.once('ready', async () => {
         });
 
         guild.save()
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     })
 
 })
