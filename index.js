77d924c6aa19b055fa9ef3f43e056417108b00ce
Felix platzer <feliplatzer06@gmail.com>
2021-12-13 20:39:58 +0100

    ok

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ client.distube
         ))
 
 
-    .on("error", (message, err) => message.channel.send(
+    .on("error", (message, err) => console.log(
         "An error encountered: " + err
     ));
 
