03d8ba6b7d6e802228debb74e613d835be317d1a
Felix platzer <feliplatzer06@gmail.com>
2021-12-13 19:23:28 +0100

    ok

--- commands/managment/ban.js
+++ commands/managment/ban.js
@@ -9,15 +9,13 @@ module.exports = {
     usage: "ban {user to ban} {reason}",
 
     async run(client, message, args, GuildPrefix) {
-        console.log(message.member);
-        console.log(message.member.permissions);
         if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) return message.reply("You dont have permission to do this!");
         if (!message.guild.me.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) return message.reply("I dont have permission to do this!");
 
         let reason = args.slice(1).join(" ");
         if (!reason) reason = "Unspecified";
 
-        const target = message.mentions.members.first() || message.guild.users.cache.get(args[0]);
+        const target = message.mentions.members.first();
 
         if (!target) {
             return message.reply(`Please mention the person who you want to ban`);
