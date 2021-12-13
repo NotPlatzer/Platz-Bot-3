492c33f41a02637ae706a701fc4f05b9f4d25ced
Felix platzer <feliplatzer06@gmail.com>
2021-12-13 14:54:21 +0100

    ok

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -12,8 +12,10 @@ module.exports = {
     async run(client, message, args, GuildPrefix) {
 
         if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+        if(!message.guild.me.voice) return message.reply("Im not in a voice channel!");
 
-        client.leaveVoiceChannel(message.member.voiceState.channel.id);
+
+        message.guild.me.voice.disconnect();
         await message.reply("**Left the Channel**");
     }
 }
