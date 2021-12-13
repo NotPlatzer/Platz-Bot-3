a8dc860a65484cd4820bfbb4b85a7499ec8215d7
Felix platzer <feliplatzer06@gmail.com>
2021-12-13 12:28:02 +0100

    l

--- commands/other/embed.js
+++ commands/other/embed.js
@@ -10,11 +10,9 @@ module.exports = {
 
         const embed = new MessageEmbed()
         .setTitle("General Information")
-        .setAuthor("Platz Bot v3", "https://yagami.xyz/content/uploads/2018/11/discord-512-1.png","https://yagami.xyz")
+        .setAuthor("Platz Bot v3")
         .setColor("GREYPLE")
-        .setDescription("This Bots lead developer is: @platz")
-        .setImage("http://i.imgur.com/yVpymuV.png")
-        .setThumbnail("https://yagami.xyz/content/uploads/2018/11/discord-512-1.png")
+        .setDescription("This Bots lead developer is: <@608381190336020494>")
         .setTimestamp()
 
         console.log(embed);
