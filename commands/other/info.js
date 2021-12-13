dd12c3122a7c7be39bf0f5c206eac6170d53b710
Felix platzer <feliplatzer06@gmail.com>
2021-12-13 13:59:08 +0100

    ok

---
+++ commands/other/info.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "info",
+    aliases: ['i'],
+    cooldown: 1000 * 5,
+    description: "Information about commands",
+
+
+    async run(client, message, args, GuildPrefix) {
+
+        
+
+    }
+
+}
