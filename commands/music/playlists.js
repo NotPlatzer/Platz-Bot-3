e681c05e9a33cf1c40bf459d2bc4d94e42894e8e
Felix platzer <feliplatzer06@gmail.com>
2021-12-14 20:58:54 +0100

    ok

--- commands/music/playlists.js
+++ commands/music/playlists.js
@@ -22,6 +22,7 @@ module.exports = {
                 const linkOfPlaylist = args[2];
 
                 if (!nameOfPlaylist) return message.reply("Please provide a name for the playlist")
+                if (nameOfPlaylist === 'list') return message.reply("Sorry, playlist name cant be list")
                 if (nameOfPlaylist.includes("https://open.spotify.com/playlist/") || nameOfPlaylist.includes("https://youtube.com/playlist")) return message.reply("The name cant be a link to a playlist")
                 if (!linkOfPlaylist) return message.reply("Please provide a Link for the playlist");
                 if (!linkOfPlaylist.includes("https://open.spotify.com/playlist/") && !linkOfPlaylist.includes("https://youtube.com/playlist")) return message.reply("Please provide a valid URL for the playlist")
@@ -44,6 +45,17 @@ module.exports = {
 
                 if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.reply("You dont have permission to do this!");
 
+                break;
+
+            case "list":
+                var playlistLIST = "";
+
+                if (messageGuild.playlists.length < 1) return message.reply("No Safed Playlists found!");
+
+                messageGuild.playlists.forEach(playlist => {
+                    playlistLIST = playlistLIST + "\n**" + playlist.name + "**";
+                })
+                message.reply(`Current playlists: ${playlistLIST}`);
                 break;
 
             default:
