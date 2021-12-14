6ce110259ccb37723700d9c8c4bcc6e45617f2eb
Felix platzer <feliplatzer06@gmail.com>
2021-12-14 20:04:30 +0100

    go balls

--- commands/music/playlists.js
+++ commands/music/playlists.js
@@ -16,8 +16,16 @@ module.exports = {
         switch (args.join(" ")) {
 
             case "add":
-
+                console.log(Guild);
                 if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.reply("You dont have permission to do this!");
+                const nameOfPlaylist = args[0];
+                const linkOfPlaylist = args[1];
+                if(!nameOfPlaylist) return message.reply("Please provide a name for the playlist")
+                if(nameofPlaylist.includes("https://open.spotify.com/playlist/") || nameofPlaylist.includes("https://youtube.com/playlist")) return message.reply("The name cant be a link to a playlist")
+                if(!linkOfPlaylist) return message.reply("Please provide a Link for the playlist");
+                if(!linkOfPlaylist.includes("https://open.spotify.com/playlist/") || !linkOfPlaylist.includes("https://youtube.com/playlist")) return message.reply("Please provide a valid URL for the playlist")
+
+                await Guild.findOneAndUpdate({}, {})
 
                 break;
 
@@ -34,7 +42,6 @@ module.exports = {
                 const result = await Guild.findOne({
                     'playlists.name': playlistName
                 })
-                console.log(result)
                 //if there is no result
                 if (result === null) {
                     return message.reply("No such playlist or you havent made any Playlists yet")
