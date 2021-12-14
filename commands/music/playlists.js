f4c170e5d415899ddc817876b8e008843a2fffe8
Felix platzer <feliplatzer06@gmail.com>
2021-12-14 18:26:36 +0100

    //

--- commands/music/playlists.js
+++ commands/music/playlists.js
@@ -18,7 +18,7 @@ module.exports = {
         })
 
         console.log('Result:', result);
-        console.log('Result playlists:', result.playlists);
+        console.log('Result playlists:', result.playlists[0]);
     }
 
 }
