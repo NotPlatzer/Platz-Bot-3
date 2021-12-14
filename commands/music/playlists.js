37e10b8fd9be71a98a7a2c8986d783ed9f15640a
Felix platzer <feliplatzer06@gmail.com>
2021-12-14 20:37:51 +0100

    kkk

--- commands/music/playlists.js
+++ commands/music/playlists.js
@@ -20,6 +20,7 @@ module.exports = {
                 if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.reply("You dont have permission to do this!");
                 const nameOfPlaylist = args[1];
                 const linkOfPlaylist = args[2];
+                console.log(nameOfPlaylist + ": " + linkOfPlaylist)
                 if (!nameOfPlaylist) return message.reply("Please provide a name for the playlist")
                 if (nameOfPlaylist.includes("https://open.spotify.com/playlist/") || nameOfPlaylist.includes("https://youtube.com/playlist")) return message.reply("The name cant be a link to a playlist")
                 if (!linkOfPlaylist) return message.reply("Please provide a Link for the playlist");
