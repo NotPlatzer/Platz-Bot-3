17f01e8d3c74b8c21dacc08c1d6f7f9f78a82650
Felix platzer <feliplatzer06@gmail.com>
2021-12-15 16:16:53 +0100

    cp logs

--- commands/managment/changeprefix.js
+++ commands/managment/changeprefix.js
@@ -13,9 +13,8 @@ module.exports = {
 
     async run(client, message, args, GuildPrefix, messageGuild) {
         if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.reply("You dont have permission to do this!");
-        console.log(Guild)
 
-        if(!args.join(" ")[0]) return message.reply("Please provide a prefix!");
+        if (!args.join(" ")[0]) return message.reply("Please provide a prefix!");
 
         const updateguild = await Guild.findOneAndUpdate({
             id: message.guild.id
@@ -27,6 +26,18 @@ module.exports = {
         );
 
         message.reply("Changed Prefix to: " + updateguild.prefix)
+        console.log(`
+        +------------------------------+
+        | Updated Guild Information    |
+        +---------+--------------------+
+        | Name:   | ${guild.name}
+        +---------+--------------------+
+        | Prefix: | ${updateguild.prefix}                  |
+        +---------+--------------------+
+        | Id:     | ${guild.id} |
+        +---------+--------------------+
+        | Owner:  | ${guild.ownerId} |
+        +---------+--------------------+`)
 
     }
 
