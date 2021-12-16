c6365bad1bb3cf1672474c788b8035500b30ef9e
Felix platzer <feliplatzer06@gmail.com>
2021-12-16 10:07:26 +0100

    ok

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -20,7 +20,7 @@ module.exports = {
         }
         else {
             await client.distube.jump(message, parseInt(args[0]))
-                .catch(err => message.channel.send("Invalid song number."));
+                .catch(err => { return message.channel.send("Invalid song number.") });
             message.reply("Jumped to song no: " + parseInt(args[0]) + " in Queue");
         }
 
