b1da1c5317fee70415c1983e4d03071db994141f
Felix platzer <feliplatzer06@gmail.com>
2021-12-16 10:21:32 +0100

    //

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -20,7 +20,7 @@ module.exports = {
         }
         else {
             console.log(parseInt(args[0]))
-            if (parseInt(args[0]) === 'NaN') return message.reply("Invalid song number.")
+            if (parseInt(args[0]) === NaN) return message.reply("Invalid song number.")
 
             await client.distube.jump(message, parseInt(args[0]))
             message.reply("Jumped to song no: " + parseInt(args[0]) + " in Queue");
