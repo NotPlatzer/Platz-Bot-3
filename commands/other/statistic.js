f8f16fab3b7ce3199dd321a18518ff879930e42d
Felix platzer <feliplatzer06@gmail.com>
2021-12-16 14:51:16 +0100

    //

--- commands/other/statistic.js
+++ commands/other/statistic.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const Guild = require('./db_models/guild');
+const Guild = require('/app/db_models/guild.js');
 
 module.exports = {
     name: "statistic",
