a733edbbb42b5dd392408b80e9f0febffba84c15
Felix platzer <feliplatzer06@gmail.com>
2021-12-21 15:42:19 +0100

    added lyrics

---
+++ commands/music/lyrics.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const lyricsFinder = require('@sujalgoel/lyrics-finder');
+
+
+module.exports = {
+    name: "lyrics",
+    aliases: ['ly'],
+    cooldown: 1000 * 5,
+    description: "Shows the Lyrics of the Current Song",
+    usage: "lyrics",
+
+
+    async run(client, message, args, GuildPrefix, messageGuild) {
+
+        const queue = client.distube.getQueue(message)
+        if (!queue) return message.reply("There is nothing to stop!");
+        if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+
+        queue.songs
+            .map((song, id) => {
+                if (id === 0) {
+                    //Do things to first song
+                    lyricsFinder.LyricsFinder(song.name).then(data => {
+                        console.log(data);
+                    });
+                }
+            })
+    }
+}
