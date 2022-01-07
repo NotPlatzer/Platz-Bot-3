46b07e3b76e35f720fe923791032fbf784fe62eb
Felix platzer <feliplatzer06@gmail.com>
2022-01-07 19:34:43 +0100

    error handeling

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Discord = require('discord.js')
-const mongoose = require('mongoose')
-const Guild = require('./db_models/guild');
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const Guild = require("./db_models/guild");
 const { MessageEmbed } = require("discord.js");
-const ms = require('ms')
-const fs = require('fs')
+const ms = require("ms");
+const fs = require("fs");
 
 const client = new Discord.Client({
-    intents: [
-        'GUILDS',
-        'GUILD_VOICE_STATES',
-        'GUILD_MESSAGES',
-        'GUILD_BANS',
-        'GUILD_MEMBERS',
-    ],
-})
+  intents: [
+    "GUILDS",
+    "GUILD_VOICE_STATES",
+    "GUILD_MESSAGES",
+    "GUILD_BANS",
+    "GUILD_MEMBERS",
+  ],
+});
 
 client.commands = new Discord.Collection();
-const commandFolders = fs.readdirSync('./commands');
+const commandFolders = fs.readdirSync("./commands");
 
 const timeout = new Discord.Collection();
 
@@ -24,35 +24,35 @@ var commandCount = 1;
 
 //Looping for every command and putting it into the "client.commands" collection
 for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
 
-    for (const file of commandFiles) {
-
-        const command = require(`./commands/${folder}/${file}`);
-        commandCount++;
-        client.commands.set(command.name.toLowerCase(), command);
-
-    }
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    commandCount++;
+    client.commands.set(command.name.toLowerCase(), command);
+  }
 }
 
 //Node error handling
-process.on('uncaughtException', function (err) {
-    console.error(err);
-    console.log("Node NOT Exiting...");
+process.on("uncaughtException", function (err) {
+  console.error(err);
+  console.log("Node NOT Exiting...");
 });
 //Discord error handling
-client.on('error', console.error);
+client.on("error", console.error);
 //gets called once the client is online
-client.once('ready', async () => {
-    //Connecting to the DB
-    await mongoose.connect(
-        process.env.DB_URI, {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+client.once("ready", async () => {
+  //Connecting to the DB
+  await mongoose
+    .connect(process.env.DB_URI, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-        .then((result) => {
-            console.log(`
+    .then((result) => {
+      console.log(`
         +-----------------------------------------+
         |              Bot is Online              |
         +-----------+-----------------------------+
@@ -62,8 +62,9 @@ client.once('ready', async () => {
         +-----------+-----------------------------+
         | Servers:  | ${client.guilds.cache.size}                           |
         +-----------+-----------------------------+`);
-        })
-        .catch((err) => console.log(`
+    })
+    .catch((err) =>
+      console.log(`
         +-----------------------------------------+
         |              Bot is Online              |
         +-----------+-----------------------------+
@@ -72,23 +73,64 @@ client.once('ready', async () => {
         | Commands: | ${commandCount - 1}                          |
         +-----------+-----------------------------+
         | Servers:  | ${client.guilds.cache.size}                           |
-        +-----------+-----------------------------+`));
-
-})
+        +-----------+-----------------------------+`)
+    );
+});
 //Gets Called once client joins a new guild
-client.on("guildCreate", guild => {
-    //Makes new db Guild Document
-    const guild_db = new Guild({
-        name: guild.name,
-        prefix: ',',
-        modRole: 'admin',
-        id: guild.id,
-        owner: guild.ownerId,
-        playlists: [],
-    });
+client.on("guildCreate", (guild) => {
+  //Makes new db Guild Document
+  const guild_db = new Guild({
+    name: guild.name,
+    prefix: ",",
+    modRole: "admin",
+    id: guild.id,
+    owner: guild.ownerId,
+    playlists: [],
+  });
 
-    guild_db.save()
-        .then((result) => {
+  guild_db
+    .save()
+    .then((result) => {
+      console.log(`
+            +------------------------------+
+            |       Joined new Guild       |
+            +---------+--------------------+
+            | Name:   | ${guild.name}      
+            +---------+--------------------+
+            | Prefix: | ,                  |
+            +---------+--------------------+
+            | Id:     | ${guild.id} |
+            +---------+--------------------+
+            | Owner:  | ${guild.ownerId} |
+            +---------+--------------------+`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Gets called once a message is received
+client.on("messageCreate", (message) => {
+  if (message.author === client.user) return;
+  if (message.channel.type === "DM") return;
+  //finds the prefix of the current Guild and puts it into the "GuildPrefix" variable
+  Guild.findOne({ id: message.guild.id })
+    .then((messageGuild) => {
+        
+      if (!messageGuild) {
+        //Makes new db Guild Document
+        const guild_db = new Guild({
+          name: guild.name,
+          prefix: ",",
+          modRole: "admin",
+          id: guild.id,
+          owner: guild.ownerId,
+          playlists: [],
+        });
+
+        guild_db
+          .save()
+          .then((result) => {
             console.log(`
             +------------------------------+
             |       Joined new Guild       |
@@ -101,135 +143,146 @@ client.on("guildCreate", guild => {
             +---------+--------------------+
             | Owner:  | ${guild.ownerId} |
             +---------+--------------------+`);
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
 
+      const GuildPrefix = messageGuild.prefix;
+      if (message.mentions.has(client.user.id)) {
+        message.reply(
+          `Hello there! My Current Prefix is: ${GuildPrefix}\nUse ${GuildPrefix}help for more Information`
+        );
+      }
+      //Return if the message doesn't start with the prefix
+      if (!message.content.startsWith(GuildPrefix)) return;
+      //"args" is a Array and it contains the arguments after the command
+      const args = message.content.slice(GuildPrefix.length).trim().split(" ");
+
+      const commandName = args.shift().toLowerCase();
+      //Find all the commands by name or alias
+      const command =
+        client.commands.get(commandName) ||
+        client.commands.find(
+          (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+        );
+
+      if (!command) return message.reply("No such Command: " + commandName);
+
+      if (command) {
+        if (command.cooldown) {
+          //If command has cooldown
+          if (timeout.has(`${command.name}${message.author.id}`))
+            return message.reply(
+              `Please Wait \`${ms(
+                timeout.get(`${command.name}${message.author.id}`) - Date.now(),
+                { long: true }
+              )}\``
+            );
+          command.run(client, message, args, GuildPrefix, messageGuild);
+          console.log(
+            `${message.author.tag} executed ${message.content} on ${message.guild.name}`
+          );
+          timeout.set(
+            `${command.name}${message.author.id}`,
+            Date.now() + command.cooldown
+          );
+          setTimeout(() => {
+            timeout.delete(`${command.name}${message.author.id}`);
+          }, command.cooldown);
+        } else {
+          //If  no cooldown
+          console.log(
+            `${message.author.tag} executed ${message.content} on ${message.guild.name}`
+          );
+          command.run(client, message, args, GuildPrefix, messageGuild);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-//Gets called once a message is received
-client.on('messageCreate', message => {
-    if (message.author === client.user) return;
-    if (message.channel.type === 'DM') return;
-    //finds the prefix of the current Guild and puts it into the "GuildPrefix" variable
-    Guild.findOne({ id: message.guild.id }).then((messageGuild) => {
-
-        if(!messageGuild) {
-           return console.log("HELLO")
-        }
-
-        const GuildPrefix = messageGuild.prefix;
-        if (message.mentions.has(client.user.id)) {
-            message.reply(`Hello there! My Current Prefix is: ${GuildPrefix}\nUse ${GuildPrefix}help for more Information`);
-        }
-        //Return if the message doesn't start with the prefix
-        if (!message.content.startsWith(GuildPrefix)) return;
-        //"args" is a Array and it contains the arguments after the command
-        const args = message.content.slice(GuildPrefix.length).trim().split(' ');
-
-        const commandName = args.shift().toLowerCase();
-        //Find all the commands by name or alias
-        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-        if (!command) return message.reply("No such Command: " + commandName);
-
-        if (command) {
-            if (command.cooldown) {
-                //If command has cooldown
-                if (timeout.has(`${command.name}${message.author.id}`)) return message.reply(`Please Wait \`${ms(timeout.get(`${command.name}${message.author.id}`) - Date.now(), { long: true })}\``);
-                command.run(client, message, args, GuildPrefix, messageGuild)
-                console.log(`${message.author.tag} executed ${message.content} on ${message.guild.name}`)
-                timeout.set(`${command.name}${message.author.id}`, Date.now() + command.cooldown);
-                setTimeout(() => {
-                    timeout.delete(`${command.name}${message.author.id}`)
-                }, command.cooldown)
-            } else {
-                //If  no cooldown
-                console.log(`${message.author.tag} executed ${message.content} on ${message.guild.name}`);
-                command.run(client, message, args, GuildPrefix, messageGuild)
-            }
-        }
-    }).catch((error) => {
-        console.log(error)
-    });
-})
-
-const distube = require('distube')
+const distube = require("distube");
 
 const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
-const playlists = require('./commands/music/playlists');
-
+const playlists = require("./commands/music/playlists");
 
 //Makes new Distube Client
 client.distube = new distube.default(client, {
-    searchSongs: 0,
-    searchCooldown: 3,
-    leaveOnEmpty: true,
-    emptyCooldown: 0,
-    leaveOnFinish: false,
-    leaveOnStop: false,
-    plugins: [new SoundCloudPlugin(), new SpotifyPlugin({ emitEventsAfterFetching: true })]
-})
+  searchSongs: 0,
+  searchCooldown: 3,
+  leaveOnEmpty: true,
+  emptyCooldown: 0,
+  leaveOnFinish: false,
+  leaveOnStop: false,
+  plugins: [
+    new SoundCloudPlugin(),
+    new SpotifyPlugin({ emitEventsAfterFetching: true }),
+  ],
+});
 
-const status = queue =>
-    `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ')
-    || 'Off'}\` | Loop: \`${queue.repeatMode
-        ? queue.repeatMode === 2
-            ? 'All Queue'
-            : 'This Song'
-        : 'Off'
-    }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
-
+const status = (queue) =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${
+    queue.filters.join(", ") || "Off"
+  }\` | Loop: \`${
+    queue.repeatMode
+      ? queue.repeatMode === 2
+        ? "All Queue"
+        : "This Song"
+      : "Off"
+  }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
 
 client.distube
-    .on('finish', queue => queue.textChannel.send('Finish queue!'))
+  .on("finish", (queue) => queue.textChannel.send("Finish queue!"))
 
-    .on('playSong', (queue, song) => {
-        const playembed = new MessageEmbed()
-            .setTitle(song.name)
-            .setColor([37, 150, 190])
-            .setDescription(`Song Duration: \`${song.formattedDuration}\`\nRequested by: ${song.user}\n\n${status(queue)}`)
-            .setFooter(`To report bugs send a message to the dev`)
-            .setImage(song.thumbnail)
+  .on("playSong", (queue, song) => {
+    const playembed = new MessageEmbed()
+      .setTitle(song.name)
+      .setColor([37, 150, 190])
+      .setDescription(
+        `Song Duration: \`${song.formattedDuration}\`\nRequested by: ${
+          song.user
+        }\n\n${status(queue)}`
+      )
+      .setFooter(`To report bugs send a message to the dev`)
+      .setImage(song.thumbnail);
 
-        queue.textChannel.send({ embeds: [playembed] });
-        console.log(`Playing ${song.name}, Requested by: ${song.user.tag}`)
+    queue.textChannel.send({ embeds: [playembed] });
+    console.log(`Playing ${song.name}, Requested by: ${song.user.tag}`);
 
-        Guild.findOne({ id: '809835346450710598' }, function (err, doc) {
-            var newsongscount = doc.playedSongs + 1;
-            doc.playedSongs = newsongscount;
-            doc.save();
-        });
-
-
-
-    })
-    .on('addSong', (queue, song) =>
-        queue.textChannel.send(
-            `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`,
-        ))
-    .on('searchNoResult', message => message.reply(`No result found!`))
-
-    .on('addList', (queue, playlist) => {
-        const listembed = new MessageEmbed()
-            .setTitle("Added Playlist:")
-            .setColor([37, 150, 190])
-            .setDescription(`**${playlist.name}**\n${playlist.songs.length} Songs added\nDuration: \`${playlist.formattedDuration}\``)
-            .setFooter(`To report bugs send a message to the dev`)
-            .setImage(playlist.thumbnail)
-
-        queue.textChannel.send({ embeds: [listembed] });
-    })
-
-
-    .on("error", (message, err) => {
-        console.log("An error encountered: " + err)
-        message.reply("An error occurred: " + err)
+    Guild.findOne({ id: "809835346450710598" }, function (err, doc) {
+      var newsongscount = doc.playedSongs + 1;
+      doc.playedSongs = newsongscount;
+      doc.save();
     });
+  })
+  .on("addSong", (queue, song) =>
+    queue.textChannel.send(
+      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    )
+  )
+  .on("searchNoResult", (message) => message.reply(`No result found!`))
 
+  .on("addList", (queue, playlist) => {
+    const listembed = new MessageEmbed()
+      .setTitle("Added Playlist:")
+      .setColor([37, 150, 190])
+      .setDescription(
+        `**${playlist.name}**\n${playlist.songs.length} Songs added\nDuration: \`${playlist.formattedDuration}\``
+      )
+      .setFooter(`To report bugs send a message to the dev`)
+      .setImage(playlist.thumbnail);
 
+    queue.textChannel.send({ embeds: [listembed] });
+  })
 
+  .on("error", (message, err) => {
+    console.log("An error encountered: " + err);
+    message.reply("An error occurred: " + err);
+  });
 
-client.login(process.env.DJS_TOKEN)
+client.login(process.env.DJS_TOKEN);
