720d0d6ad685d5558dd9067b5f60321fc8e82654
Felix platzer <feliplatzer06@gmail.com>
2022-01-09 11:53:45 +0100

    ...

--- commands/other/test.js
+++ commands/other/test.js
@@ -14,6 +14,7 @@ module.exports = {
     if (message.author.id !== "608381190336020494") return;
 
     const filter = (m) => m.author.id === message.author.id;
+    console.log(filter);
     message.reply(
       `⚠This will cause everyone that has the new OR old muterole to be Muted!!!!\nType YES or NO (Will expire in 10 seconds)`
     );
