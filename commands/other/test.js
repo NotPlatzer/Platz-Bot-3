98408a542000f9b88d1a22e3eb25990969481c2b
Felix platzer <feliplatzer06@gmail.com>
2022-01-09 12:01:59 +0100

    oke

--- commands/other/test.js
+++ commands/other/test.js
@@ -14,14 +14,14 @@ module.exports = {
     if (message.author.id !== "608381190336020494") return;
 
     const filter = (m) => m.author.id === message.author.id;
-    console.log(filter);
     message.reply(
       `⚠This will cause everyone that has the new OR old muterole to be Muted!!!!\nType YES or NO (Will expire in 10 seconds)`
     );
     message.channel
       .awaitMessages({ filter, max: 1, time: 1_000, errors: ["time"] })
       .then(async (collected) => {
-        console.log(collected.first());
+        var msg = collected.first().content;
+        collected.first().reply(msg);
       })
       .catch((err) => {
         console.log(err);
