b9f3853196b7101e89c97ab2ae44d8079d801e6b
Felix platzer <feliplatzer06@gmail.com>
2022-01-10 14:43:40 +0100

    oek

--- commands/other/mc.js
+++ commands/other/mc.js
@@ -1,21 +1,29 @@
 const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
 const commandFolders = fs.readdirSync("commands");
+const mc = require("minecraft-server-status-simple");
 
 module.exports = {
   name: "mc",
   aliases: [""],
   cooldown: 1000 * 0,
   description: "Shows Status and Info about a MC server",
-  usage: "mc {ip} {optional port}",
+  usage: "mc {ip}:{optional port}",
 
   async run(client, message, args, GuildPrefix, messageGuild) {
-    var ip = args[0];
-    var port = args[1];
+    var data = args[0].split(":");
 
-    console.log(ip + " " + port);
+    var port = "";
+    var ip = data[0];
+    if (data.length > 1) {
+      var port = data[1];
+    }
 
-    mc.statusJava("play.cubecraft.net", 25565)
+    if (port == "") {
+      port = 25565;
+    }
+
+    mc.statusJava(ip, port)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   },
