9a3e5109465ba8d8e3a13c33aff8a3aeaec1f4aa
Felix platzer <feliplatzer06@gmail.com>
2022-01-12 20:45:43 +0100

    klbjvhg

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,23 +1,25 @@
 const Discord = require("discord.js");
 
-
 module.exports = {
-    name: "play",
-    aliases: ['p'],
-    cooldown: 0,
-    description: "Plays a song/playlist",
-    usage: "play {song/playlist}",
+  name: "play",
+  aliases: ["p"],
+  cooldown: 0,
+  description: "Plays a song/playlist",
+  usage: "play {song/playlist}",
 
-
-    async run (client, message, args, GuildPrefix, messageGuild) {
-        if(!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
-        if(client.voiceState.channelid !== message.member.voice.channelid) return message.reply("You have to be in the same voice channel as the Bot!")
-
-        const music = args.join(" ");
-        if(!music) return message.reply("Please provide a Song!");
-
-        await client.distube.play(message, music);
-        
+  async run(client, message, args, GuildPrefix, messageGuild) {
+    if (!message.member.voice.channel)
+      return message.reply("You have to be in a voice channel!");
+      
+    if (client.voiceState !== undefined) {
+      if (client.voiceState.channelid !== message.member.voice.channelid)
+        return message.reply(
+          "You have to be in the same voice channel as the Bot!"
+        );
     }
+    const music = args.join(" ");
+    if (!music) return message.reply("Please provide a Song!");
 
-}
+    await client.distube.play(message, music);
+  },
+};
