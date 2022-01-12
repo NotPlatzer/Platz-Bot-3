1b855219e9b1d2307570f6973b23338d64064b01
Felix platzer <feliplatzer06@gmail.com>
2022-01-12 10:18:26 +0100

    tgsd

--- commands/other/mc.js
+++ commands/other/mc.js
@@ -26,68 +26,76 @@ module.exports = {
     mc.statusJava(ip, port)
       .then((server) => {
         const serverembed = new MessageEmbed();
-
-        if (server.online == false) {
-          serverembed
-            .setAuthor(
-              "Platz Bot v3",
-              "https://cdn.discordapp.com/avatars/917878990478377020/7f147973452d4a6bacbb6132b8e4a18d.png"
-            )
-            .setFooter(`To report bugs send a message to the dev`)
-            .setColor([255, 0, 0])
-            .setTitle(`${server.ip}`)
-            .addField(`Online:`, `${server.online}\n`);
-        } else {
-          serverembed
-            .setAuthor(
-              "Platz Bot v3",
-              "https://cdn.discordapp.com/avatars/917878990478377020/7f147973452d4a6bacbb6132b8e4a18d.png"
-            )
-            .setFooter(`To report bugs send a message to the dev`);
-          if (server.motd !== undefined) {
-            serverembed.setDescription(`${server.motd.clean}`);
-          }
-          if (server.ip !== undefined) {
-            serverembed.setTitle(`${server.ip}`);
-          }
-
-          if (server.players !== undefined) {
-            if (
-              server.players.online > 0 &&
-              server.players.list !== undefined
-            ) {
-              var players = server.players.list;
-              if (players.length > 9) {
-                var playersOnDisplay = players.slice(0, 9);
-                playersOnDisplay = playersOnDisplay.join("\r\n");
-                var numberOfPlayersNotOnDisplay = players.length - 9;
-                var playersFORMATED = `
-                ${playersOnDisplay}\n\`+ ${numberOfPlayersNotOnDisplay}\``;
-              } else {
-                var playersFORMATED = players.join("\r\n");
-              }
-
-              serverembed.addField(
-                `Players: ${server.players.online + "/" + server.players.max}`,
-                `${playersFORMATED}`
-              );
-            } else if (server.players !== undefined) {
-              serverembed.addField(
-                `Players: ${server.players.online + "/" + server.players.max}`,
-                `/`
-              );
+        if (server.online == undefined) {
+          if (server.online == false) {
+            serverembed
+              .setAuthor(
+                "Platz Bot v3",
+                "https://cdn.discordapp.com/avatars/917878990478377020/7f147973452d4a6bacbb6132b8e4a18d.png"
+              )
+              .setFooter(`To report bugs send a message to the dev`)
+              .setColor([255, 0, 0])
+              .setTitle(`${server.ip}`)
+              .addField(`Online:`, `${server.online}\n`);
+          } else {
+            serverembed
+              .setAuthor(
+                "Platz Bot v3",
+                "https://cdn.discordapp.com/avatars/917878990478377020/7f147973452d4a6bacbb6132b8e4a18d.png"
+              )
+              .setFooter(`To report bugs send a message to the dev`);
+            if (server.motd !== undefined) {
+              serverembed.setDescription(`${server.motd.clean}`);
             }
-          }
-          if (server.version !== undefined) {
-            serverembed.addField(`Version:`, `${server.version}`);
-          }
-          if (server.software !== undefined) {
-            serverembed.addField(`Software:`, `${server.software}`);
-          }
+            if (server.ip !== undefined) {
+              serverembed.setTitle(`${server.ip}`);
+            }
 
-          serverembed
-            .addField(`Online:`, `${server.online}\n`)
-            .setColor([71, 122, 30]);
+            if (server.players !== undefined) {
+              if (
+                server.players.online > 0 &&
+                server.players.list !== undefined
+              ) {
+                var players = server.players.list;
+                if (players.length > 9) {
+                  var playersOnDisplay = players.slice(0, 9);
+                  playersOnDisplay = playersOnDisplay.join("\r\n");
+                  var numberOfPlayersNotOnDisplay = players.length - 9;
+                  var playersFORMATED = `
+                ${playersOnDisplay}\n\`+ ${numberOfPlayersNotOnDisplay}\``;
+                } else {
+                  var playersFORMATED = players.join("\r\n");
+                }
+
+                serverembed.addField(
+                  `Players: ${
+                    server.players.online + "/" + server.players.max
+                  }`,
+                  `${playersFORMATED}`
+                );
+              } else if (server.players !== undefined) {
+                serverembed.addField(
+                  `Players: ${
+                    server.players.online + "/" + server.players.max
+                  }`,
+                  `/`
+                );
+              }
+            }
+            if (server.version !== undefined) {
+              serverembed.addField(`Version:`, `${server.version}`);
+            }
+            if (server.software !== undefined) {
+              serverembed.addField(`Software:`, `${server.software}`);
+            }
+
+            serverembed
+              .addField(`Online:`, `${server.online}\n`)
+              .setColor([71, 122, 30]);
+          }
+        }
+        else{
+          serverembed.addField(`Could Not Find any Information`, `<------------------------------->`);
         }
         message.reply({ embeds: [serverembed] });
       })
