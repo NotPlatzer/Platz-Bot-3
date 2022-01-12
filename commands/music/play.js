8bdd4807f5a8af05873d7916d0753d56e5ce352b
Felix platzer <feliplatzer06@gmail.com>
2022-01-12 20:41:54 +0100

    oke

--- commands/music/play.js
+++ commands/music/play.js
@@ -11,6 +11,7 @@ module.exports = {
 
     async run (client, message, args, GuildPrefix, messageGuild) {
         if(!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+        if(client.voiceState.channelid !== message.member.voice.channelid) return message.reply("You have to be in the same voice channel as the Bot!")
 
         const music = args.join(" ");
         if(!music) return message.reply("Please provide a Song!");
