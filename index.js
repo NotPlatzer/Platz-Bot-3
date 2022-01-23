1fb971d58ca96fa229f1c0b86891a90646eedd41
Felix platzer <feliplatzer06@gmail.com>
2022-01-23 13:48:45 +0100

    oke

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const mcServer = schedule.scheduleJob("10 * * * * *", function (fireDate) {
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
           var server = JSON.parse(body);
-          console.log(server);
           if (server.PlayerList === false || server === undefined) return;
           let PlayersOnServer = server.PlayerList;
           const namesToDeleteSet = new Set(players);
