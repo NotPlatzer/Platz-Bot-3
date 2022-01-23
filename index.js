615c8c50ba2c721df9f25d58ad2d44a5eb8acf28
Felix platzer <feliplatzer06@gmail.com>
2022-01-23 15:45:40 +0100

    ahso

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const mcServer = schedule.scheduleJob("10 * * * * *", function (fireDate) {
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
           var server = JSON.parse(body);
-          console.log(server.PlayerList);
           if (server.PlayerList === false || server === undefined) return;
           let PlayersOnServer = server.PlayerList;
           const namesToDeleteSet = new Set(players);
@@ -24,12 +23,11 @@ const mcServer = schedule.scheduleJob("10 * * * * *", function (fireDate) {
             return !namesToDeleteSet.has(name);
           });
           console.log(newPlayers);
-          console.log(PlayersOnServer);
           if (newPlayers.length > 0) {
             doc.mcPlayers = players.concat(newPlayers);
             doc.save();
             var mcPlayersChannel = client.channels
-              .fetch("919218030746148884")
+              .fetch("928347331596808302")
               .then((channel) => {
                 var counter = newPlayers.length;
                 newPlayers.forEach((player) => {
