51831d3d5724aaa99b5684c358f6e0b5a6efc5f0
Felix platzer <feliplatzer06@gmail.com>
2022-01-31 21:45:06 +0100

    fixed error with filter command

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -8,6 +8,19 @@ module.exports = {
   usage: "filter {3d, bassboost, echo, karaoke, nightcore, vaporwave}",
 
   async run(client, message, args, GuildPrefix, messageGuild) {
+    const queue = client.distube.getQueue(message);
+    if (!queue) return message.reply("There is no Queue");
+    if (!message.member.voice.channel)
+      return message.reply("You have to be in a voice channel!");
+    if (message.guild.me.voice.channel !== null) {
+      if (
+        message.member.voice.channel.id !== message.guild.me.voice.channel.id
+      ) {
+        return message.reply(
+          "You have to be in the same Voice Channel as the Bot!"
+        );
+      }
+    }
     if (
       [`3d`, `bassboost`, `echo`, `karaoke`, `nightcore`, `vaporwave`].includes(
         args[0]
