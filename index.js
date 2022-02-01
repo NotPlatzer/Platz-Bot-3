e1d7eaac41711488d38d41d31b1315f3b27e530c
Felix platzer <feliplatzer06@gmail.com>
2022-02-01 15:28:49 +0100

    opiklj

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ client.on("error", () => {
 });
 //Gets called once the client is online
 client.once("ready", async () => {
-  client.user.setActivity("discord.js", { type: "WATCHING" });
+  client.user.setActivity(`${client.guilds.cache.size} servers`, { type: "WATCHING" });
   //Connecting to the DB
   await mongoose
     .connect(process.env.DB_URI, {
