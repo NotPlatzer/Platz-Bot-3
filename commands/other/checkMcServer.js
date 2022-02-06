b748f87f426d595764d4214e00e9f3285a570580
Felix platzer <feliplatzer06@gmail.com>
2022-02-06 22:04:05 +0100

    ok

--- commands/other/checkMcServer.js
+++ commands/other/checkMcServer.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   name: "checkMcServer",
   aliases: [""],
@@ -6,7 +5,48 @@ module.exports = {
   description: "",
   usage: "",
 
-  async run(client, message, args, GuildPrefix, messageGuild) {
-   console.log("hello")
+  async run(client, fireDate) {
+    Guild.findOne({ id: "809835346450710598" }, function (err, doc) {
+      console.log(fireDate);
+      if (doc == undefined) return console.log("Could not find Data Guild!");
+      const players = doc.mcPlayers;
+      const server = request.get(
+        "https://api.mcsrvstat.us/debug/query/5.83.164.91:10050",
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var server = JSON.parse(body);
+            if (
+              server.PlayerList === false ||
+              server === undefined ||
+              server.PlayerList === undefined
+            )
+              return;
+            let PlayersOnServer = server.PlayerList;
+            const namesToDeleteSet = new Set(players);
+            const newPlayers = PlayersOnServer.filter((name) => {
+              return !namesToDeleteSet.has(name);
+            });
+            if (newPlayers.length > 0) {
+              console.log(newPlayers);
+              doc.mcPlayers = players.concat(newPlayers);
+              doc.save();
+              var mcPlayersChannel = client.channels
+                .fetch("928347331596808302")
+                .then((channel) => {
+                  var counter = newPlayers.length;
+                  newPlayers.forEach((player) => {
+                    counter--;
+                    channel.send(
+                      `${doc.mcPlayers.length - counter}. \`${player}\``
+                    );
+                  });
+                });
+            } else {
+              return;
+            }
+          }
+        }
+      );
+    });
   },
 };
--- index.js
+++ index.js
@@ -342,48 +342,7 @@ client.distube
 //Gets executed once a minute and checks for new players on The MC server
 schedule.scheduleJob("10 * * * * *", function (fireDate) {
   const checkMcserver = client.commands.get("checkmcserver");
-  checkMcserver.run(client);
-  Guild.findOne({ id: "809835346450710598" }, function (err, doc) {
-    if (doc == undefined) return console.log("Could not find Data Guild!");
-    const players = doc.mcPlayers;
-    const server = request.get(
-      "https://api.mcsrvstat.us/debug/query/5.83.164.91:10050",
-      function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var server = JSON.parse(body);
-          if (
-            server.PlayerList === false ||
-            server === undefined ||
-            server.PlayerList === undefined
-          )
-            return;
-          let PlayersOnServer = server.PlayerList;
-          const namesToDeleteSet = new Set(players);
-          const newPlayers = PlayersOnServer.filter((name) => {
-            return !namesToDeleteSet.has(name);
-          });
-          if (newPlayers.length > 0) {
-            console.log(newPlayers);
-            doc.mcPlayers = players.concat(newPlayers);
-            doc.save();
-            var mcPlayersChannel = client.channels
-              .fetch("928347331596808302")
-              .then((channel) => {
-                var counter = newPlayers.length;
-                newPlayers.forEach((player) => {
-                  counter--;
-                  channel.send(
-                    `${doc.mcPlayers.length - counter}. \`${player}\``
-                  );
-                });
-              });
-          } else {
-            return;
-          }
-        }
-      }
-    );
-  });
+  checkMcserver.run(client, fireDate);
 });
 
 //Loging in to the Discord Client
