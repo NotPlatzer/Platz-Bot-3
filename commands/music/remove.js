78f4e50ea09ce5bf8df43a9798d915ecc14652d2
Felix platzer <feliplatzer06@gmail.com>
2022-03-18 09:17:17 +0100

    ok

---
+++ commands/music/remove.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const { Permissions } = require("discord.js");
+
+module.exports = {
+  name: "remove",
+  aliases: ["rm", "rem"],
+  cooldown: 1000 * 0,
+  description: "Removes a specific song from the queue",
+  usage: "remove {index in queue}",
+  ownerOnly: false,
+
+  async run(client, message, args, GuildPrefix, messageGuild) {
+    if (!message.member.voice.channel)
+      return message.reply("You have to be in a voice channel!");
+    if (!message.guild.me.voice.channel)
+      return message.reply("Im not in a voice channel!");
+    if (message.guild.me.voice.channel !== null) {
+      if (
+        message.member.voice.channel.id !== message.guild.me.voice.channel.id
+      ) {
+        return message.reply(
+          "You have to be in the same Voice Channel as the Bot!"
+        );
+      }
+    }
+    const queue = client.distube.getQueue(message);
+    if(!queue) {
+        return message.reply(`There is no queue`)
+    }
+    if (isNaN(args[0])) return message.reply("Invalid song number");
+    const rmno = parseInt(args[0]);
+    queue.songs.splice(1, rmno)
+  },
+};
