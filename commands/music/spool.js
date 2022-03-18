618a12ae87160f4c1943187cc5c6eb8f7e2c2ff1
Felix platzer <feliplatzer06@gmail.com>
2022-03-18 11:57:59 +0100

    ok

---
+++ commands/music/spool.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const { Permissions } = require("discord.js");
+
+module.exports = {
+  name: "spool",
+  aliases: ["spo", "spol"],
+  cooldown: 1000 * 0,
+  description: "Spools to a certain time in the current song",
+  usage: "spool {seconds}",
+  ownerOnly: false,
+
+  async run(client, message, args, GuildPrefix, messageGuild) {
+    if (!message.member.voice.channel)
+      return message.reply("You have to be in a voice channel!");
+    if (!message.guild.me.voice.channel)
+      return message.reply("Im not in a voice channel!");
+    if (message.guild.me.voice.channel !== null) {
+      if (
+        message.member.voice.channel.id !== message.guild.me.voice.channel.id
+      ) {
+        return message.reply(
+          "You have to be in the same Voice Channel as the Bot!"
+        );
+      }
+    }
+    const queue = client.distube.getQueue(message);
+    if (!queue) {
+      return message.reply(`There is nothing playing right now`);
+    }
+    if (isNaN(args[0])) return message.reply("Invalid number of seconds");
+    const seconds = parseInt(args[0]);
+    const currentTime = queue.currentTime;
+    const duration = queue.songs[0].duration;
+    //logic for spooling in the song
+    console.log(`c: ${currentTime}\nd: ${duration}`);
+    return message.reply(
+      `Skipped to \`${seconds}\` seconds in \`${queue.songs[0]}\``
+    );
+  },
+};
