99b81f953e4b9937817043687ad129f108ffd1d4
Felix platzer <feliplatzer06@gmail.com>
2022-04-07 15:32:58 +0200

    k

---
+++ commands/other/test.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const Guild = require("/app/db_models/guild.js");
+const jimp = require("jimp");
+const fs = require("fs");
+
+module.exports = {
+  name: "move",
+  aliases: ["test"],
+  cooldown: 1000 * 0,
+  description: "Changes the prefix",
+  usage: "changePrefix {new prefix}",
+  ownerOnly: true,
+  category: "other",
+
+  async run(client, message, args, GuildPrefix, messageGuild) {
+    message.channel.send("!resign @NotPlatzer#1106");
+    sleep(2000);
+    message.channel.send("!chess @NotPlatzer#1106");
+  },
+};
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
